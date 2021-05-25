5bfe84a6cf7a595ac27bdf4b75c6abcee59e73b2
soumya997 <soumya997.sarkar@gmail.com>
2021-05-26 01:25:20 +0530

    added day 6

---
+++ Day_6/cout_to_print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#define print(x) cout << x
+// #define endline endl
+
+int main()
+{
+    print("Hello World!")<<endl;
+    print("hi");
+    // print(endline);
+
+    return 0;  
+}
