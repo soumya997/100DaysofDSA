01d75d33f1ba39490602b797f0ebaaf947f81975
Soumyadip Sarkar <54326088+soumya997@users.noreply.github.com>
2021-08-09 22:28:28 +0530

    Update bubble_sort.cpp

--- Day_2/bubble_sort.cpp
+++ Day_2/bubble_sort.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 void bubble_sort(int arr[],int n){
     //take the bigger at the end
-    for(int i=1;i<n;i++){
-        for(int j=0;j<=n-2;j++){
+    for(int i=1;i<=n-1;i++){
+        for(int j=0;j<=(n-itr-1);j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
             }
